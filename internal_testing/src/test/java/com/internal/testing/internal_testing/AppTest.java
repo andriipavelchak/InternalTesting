f34452650d10585ab1d5081f679a0f8b53d4711b
apavel <apavel@ch156.softservecom.com>
2018-04-23 17:24:02 +0300

    Internal test commit...

---
+++ internal_testing/src/test/java/com/internal/testing/internal_testing/AppTest.java
@@ -0,0 +1,31 @@
+package com.internal.testing.internal_testing;
+	
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.chrome.ChromeDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class AppTest {		
+	    private WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+			driver.get("http://demo.guru99.com/test/guru99home/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+			System.setProperty("webdriver.chrome.driver", "C:\\Development\\tools\\Selenium\\chromedriver_win32\\chromedriver.exe");
+			
+			driver = new ChromeDriver();  
+			
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
